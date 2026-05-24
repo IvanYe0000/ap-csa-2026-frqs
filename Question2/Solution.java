d49b9f66068e6068d1d58f32457fab8a69a53e47
IvanYe0000 <ivanye0618@gmail.com>
2026-05-24 17:15:09 -0400

    Update Solution.java

--- Question2/Solution.java
+++ Question2/Solution.java
@@ -1,1 +1,17 @@
+public class Bottle{
+  private double capacity
+  private double amount
 
+  public Bottle(double n, double c){
+    this.capacity = n;
+    this.amount  = c;
+  }
+
+  public double updateAMount(double used){
+    amount -= used;
+    if (amount < capacity * 0.25){
+      amount = capcity;
+    }
+  return amount;
+}
+    
