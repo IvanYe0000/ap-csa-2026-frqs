9d80ad75ea99a9dbfe7f934ab523c88898c0fbe4
IvanYe0000 <ivanye0618@gmail.com>
2026-05-24 19:07:34 -0400

    Update Solution.java

--- Question3/Solution.java
+++ Question3/Solution.java
@@ -1,1 +1,12 @@
-
+public int moreHistoryThanMathAbsence(){
+  int count = 0;
+  for( Courserecord c : historyList){
+    for (courserfecord m :mathList){
+      if (c.getStudentID().equals(m.getStudentID()) && c.getAbsence() > m.getAbsence()){
+        count++;
+      }
+    }
+  }
+  return count;
+}
+  
