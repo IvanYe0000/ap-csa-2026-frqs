1101471af44aecbee41c4d554b27547246fdbef4
IvanYe0000 <ivanye0618@gmail.com>
2026-05-24 17:11:59 -0400

    Update Part-B.java

--- Question1/Part-B.java
+++ Question1/Part-B.java
@@ -1,1 +1,11 @@
-
+public string getShortenName(){
+  string result = "";
+  for (int i = 0; i < username.length(); i++){
+    if (username.charAt(i) != '-'){
+      result += username.charAt(i){
+    else{
+      result = result.substring(0, result.length() - 1);
+    }
+  }
+  return result
+}
