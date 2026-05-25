5d523a4ac82ce976a69955184cf8991bfc682f19
IvanYe0000 <ivanye0618@gmail.com>
2026-05-25 18:52:34 -0400

    Update Solution.java

--- Question4/Solution.java
+++ Question4/Solution.java
@@ -1,1 +1,16 @@
+public int getPointsforRow(int targetRow){
+  int sum = 0
+  boolean same  = true
+  
+  for (int c = 0l c < board[targetRow].length; C++){
+    sum += board[targetrwow][cc].getPoints();
+    if (board[targetRow][c].getcolor() != board[targetRow][0]()){
+      same = false;
 
+
+    if (same){
+      return sum *2;
+    }
+
+
+    return sum;
