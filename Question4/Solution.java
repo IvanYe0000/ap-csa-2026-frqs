260e86d8a41599bfcbbabdf857b0f50edcc03c35
Joshua Hans <152180694+hansTeachesCS@users.noreply.github.com>
2026-05-26 10:10:11 -0400

    Correct getPointsforRow method implementation
    Fix syntax errors and improve logic in getPointsforRow method.

--- Question4/Solution.java
+++ Question4/Solution.java
@@ -2,15 +2,15 @@ public int getPointsforRow(int targetRow){
   int sum = 0
   boolean same  = true
   
-  for (int c = 0l c < board[targetRow].length; C++){
-    sum += board[targetrwow][cc].getPoints();
-    if (board[targetRow][c].getcolor() != board[targetRow][0]()){
+  for (int c = 0; c < board[targetRow].length; c++){
+    sum += board[targetrwow][c].getPoints();
+    if (board[targetRow][c].getcolor() != board[targetRow][0].getColor()){
       same = false;
-
+    }
 
     if (same){
       return sum *2;
     }
 
-
     return sum;
+}
